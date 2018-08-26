6e778630f9fbea7fb9f1ddf1ee37f020142433ae
Matea Bjorklund <mateabjork@gmail.com>
2018-08-26 17:37:33 +0200

    update

---
+++ cmds.js
@@ -0,0 +1,5 @@
+//const commandModule = module.exports = {};
+const main = require('./bot.js');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
