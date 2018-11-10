bea327aa124c4ebe8a04d6fe2f477c724c9e5842
matea <mateabjork@gmail.com>
2018-11-10 22:36:43 +0100

    bug fixes

--- server.js
+++ server.js
@@ -200,7 +200,7 @@ client.on("message", async(message) => {
          message.channel.send("You need to specify a yes or no question! ex: `t.8ball should i go to sleep?`");
        } else {
          var int =  Math.floor(Math.random() * Math.floor(12));
-         const keys = require('./resources/eightball');
+         const keys = require('.//commands/eightball');
          var intString = int.toString();
          message.channel.send(keys[intString]);
        }
