5d8aa4165af2f929c100d22f31331408d974a787
matea <mateabjork@gmail.com>
2018-11-22 22:34:37 +0100

    updated command logger

--- src/events/message.js
+++ src/events/message.js
@@ -9,14 +9,16 @@ module.exports = (client, message, Discord)  => {
         require("./../core/responder.js").fn(client, message, responses);
     }
   }
+
   if (!(message.content.startsWith(guildConf.prefix))) return;
 
   //MESSAGE IS A COMMAND, RUN COMMAND
   const args = message.content.slice(guildConf.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
+
   if(client.commands.get(command)) {
-    client.commands.get(command).fn(client, message, args);
-    console.log(`Ran command: ${command}, with argument(s): ${args}\nRequested by user: ${message.author}\nIn guild: ${message.guild.id}\n`);
+    client.commands.get(command).fn(client, message, args, Discord);
+    console.log(`\nRan command: ${command}, with argument(s): ${args}\nRequested by user: ${message.author.tag} / ${message.author} \nIn guild: ${message.guild.name} / ${message.guild.id}`);
   }
 }
