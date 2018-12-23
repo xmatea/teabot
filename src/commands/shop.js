1848cb594ca28610d0034fc91d5bdc69c690b65e
matea <mateabjork@gmail.com>
2018-12-23 01:55:12 +0100

    refactor

--- src/commands/shop.js
+++ src/commands/shop.js
@@ -9,142 +9,88 @@ exports.meta = {
 exports.fn = function (client, message, args, guild) {
     const Discord = require('discord.js');
     const User = require('./../core/models/user.js');
-    const items = require("./../lib/items/items.js");
-
-    let p = guild.config.prefix;
+    const itemObj = require("./../lib/items/items.js");
+    const crates = require("./../core/crates.js")
     let errmsg = "An error occurred! Contact **cursedtea#5140**";
-    let nodoc = "Could not inc document for " + message.author.id;
+    let nodoc = "Could not find document for " + message.author.id;
+    let p = guild.config.prefix;
 
     if (args === undefined || args.length == 0) {
         message.channel.send(new Discord.RichEmbed()
             .setTitle("Shop")
+            .setColor("A96075")
             .setDescription(`Welcome to the store **${message.author.username}**!\nTo buy an item, type \`${p}shop buy <item>\`\nFor more info, type \`${p}shop help\``)
-            .addField("Crates", "small crate: **75** :cherry_blossom:\nmedium crate: **90** :cherry_blossom:\nlarge crate: **100** :cherry_blossom:"));
+            .addField("Crates", `small crate: **${crates.smallCrate.price}** :cherry_blossom:\nmedium crate: **${crates.meduimCrate.price}** :cherry_blossom:\nlarge crate: **${crates.largeCrate.price}** :cherry_blossom:`));
     }
 
     if (args[0] === "buy") {
         if (args[2] === "crate") {
-            if (args[1] === "small") {
-                User.findById(message.author.id, function (err, doc) {
-                    if (err) { message.channel.send(errmsg); console.log(err); return }
-                    if (!doc) { message.channel.send(errmsg); console.log(nodoc); return }
-                    if (doc.bank.bal < 75) { message.channel.send("You need more money!"); return }
-                    let item1 = getObj();
-                    let item2 = getObj();
+            var crate;
+            var items = [];
+            var itemNames = [];
 
-                    User.updateOne(
-                        { _id: message.author.id },
-                        {
-                            $set:
-                                { "bank.bal": doc.bank.bal - 75 },
-                            $push: { inventory: { $each: [item1.name, item2.name] } }
-                        }, function (err, doc) {
-                            if (err) {
-                                message.channel.send(errmsg);
-                                console.log(err);
-                            }
-                            doc.save;
+            if (args[1] === "small") { crate = crates.smallCrate; }
+            if (args[1] == "medium") { crate = crates.mediumCrate; }
+            if (args[1] == "large") { crate = crates.largeCrate; }
+
+            for (let i = 0; i < crate.size; i++) {
+                let obj = getObj();
+                items.push(obj);
+                itemNames.push(obj.name);
+            }
+
+            User.findById(message.author.id, function (err, doc) {
+                if (err) { message.channel.send(errmsg); console.log(err); return }
+                if (!doc) { message.channel.send(errmsg); console.log(nodoc); return }
+                if (doc.bank.bal < crate.price) { message.channel.send("You need more money!"); return }
+
+                User.updateOne(
+                    { _id: message.author.id },
+                    {
+                        $set: { "bank.bal": doc.bank.bal - crate.price },
+                        $push: { inventory: itemNames }
+                    }, function (err, doc) {
+                        if (err) {
+                            message.channel.send(errmsg);
+                            console.log(err);
+                        } doc.save;
+                    });
+
+                if (message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+                    message.channel.send(new Discord.RichEmbed()
+                        .setDescription(`**${message.author.username}** just bought a **${crate.name}**!`)
+                        .setColor("A96075"))
+                        .then((message) => {
+                            setTimeout(function () {
+                                message.edit(new Discord.RichEmbed()
+                                    .setDescription(`Opening the crate...`)
+                                    .setColor("A96075"))
+                            }, 1500);
+                            message.delete(5000);
                         });
 
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [1/2]")
-                        .setDescription("1x **" + item1.value + "** " + item1.name)
-                        .setThumbnail(item1.img64));
-
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [2/2]")
-                        .setDescription("1x **" + item2.value + "** " + item2.name)
-                        .setThumbnail(item2.img64));
-                });
-            }
-            if (args[1] === "medium") {
-                User.findById(message.author.id, function (err, doc) {
-                    if (err) { message.channel.send(errmsg); console.log(err); return }
-                    if (!doc) { message.channel.send(errmsg); console.log(nodoc); return }
-                    if (doc.bank.bal < 90) { message.channel.send("You need more money!"); return }
-                    let item1 = getObj();
-                    let item2 = getObj();
-                    let item3 = getObj();
-
-                    User.updateOne(
-                        { _id: message.author.id },
-                        {
-                            $set:
-                                { "bank.bal": doc.bank.bal - 90 },
-                            $push: { inventory: { $each: [item1.name, item2.name, item3.name] } }
-                        }, function (err, doc) {
-                            if (err) {
-                                message.channel.send(errmsg);
-                                console.log(err);
-                            }
-                            doc.save;
-                        });
-
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [1/3]")
-                        .setDescription("1x **" + item1.value + "** " + item1.name)
-                        .setThumbnail(item1.img64));
-
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [2/3]")
-                        .setDescription("1x **" + item2.value + "** " + item2.name)
-                        .setThumbnail(item2.img64));
-                    
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [3/3]")
-                        .setDescription("1x **" + item3.value + "** " + item3.name)
-                        .setThumbnail(item3.img64));
-                });
-            }
-            if (args[1] === "large") {
-                User.findById(message.author.id, function (err, doc) {
-                    if (err) { message.channel.send(errmsg); console.log(err); return }
-                    if (!doc) { message.channel.send(errmsg); console.log(nodoc); return }
-                    if (doc.bank.bal < 100) { message.channel.send("You need more money!"); return }
-                    let item1 = getObj();
-                    let item2 = getObj();
-                    let item3 = getObj();
-                    let item4 = getObj();
-
-                    User.updateOne(
-                        { _id: message.author.id },
-                        {
-                            $set:
-                                { "bank.bal": doc.bank.bal - 100 },
-                            $push: { inventory: { $each: [item1.name, item2.name, item3.name, item4.name] } }
-                        }, function (err, doc) {
-                            if (err) {
-                                message.channel.send(errmsg);
-                                console.log(err);
-                            }
-                            doc.save;
-                        });
-
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [1/4]")
-                        .setDescription("1x **" + item1.value + "** " + item1.name)
-                        .setThumbnail(item1.img64));
-
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [2/4]")
-                        .setDescription("1x **" + item2.value + "** " + item2.name)
-                        .setThumbnail(item2.img64));
-                    
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [3/4]")
-                        .setDescription("1x **" + item3.value + "** " + item3.name)
-                        .setThumbnail(item3.img64));
-                    
-                    message.channel.send(new Discord.RichEmbed()
-                        .setTitle("You recieved an item! [4/4]")
-                        .setDescription("1x **" + item4.value + "** " + item4.name)
-                        .setThumbnail(item4.img64));
-                });
-            }
+                    setTimeout(() => {
+                        for (let i = 0; i < items.length; i++) {
+                            message.channel.send(new Discord.RichEmbed()
+                                .setTitle(`You recieved an item! [${i + 1}/${crate.size}]`)
+                                .setDescription(`1x **${items[i].value}** ${items[i].name}`)
+                                .setColor("A96075")
+                                .setThumbnail(items[i].img64));
+                        }
+                    }, 5000)
+                } else {
+                    for (let i = 0; i < items.length; i++) {
+                        message.channel.send(new Discord.RichEmbed()
+                            .setTitle(`You recieved an item! [${i + 1}/${crate.size}]`)
+                            .setDescription(`1x **${items[i].value}** ${items[i].name}`)
+                            .setColor("A96075")
+                            .setThumbnail(items[i].img64));
+                    }
+                }
+            });
         }
 
         function getObj() {
-            let obj;
             function weightedRand(spec) {
                 var i, sum = 0, r = Math.random();
 
@@ -154,19 +100,21 @@ exports.fn = function (client, message, args, guild) {
                 }
             }
 
+            let obj;
             let j = weightedRand({ 0: 0.6, 1: 0.3, 2: 0.07, 3: 0.03 });
+
             if (j == 0) {
-                obj = items.common;
-                return items.common[Math.floor(Math.random() * obj.length)];
+                obj = itemObj.common;
+                return itemObj.common[Math.floor(Math.random() * obj.length)];
             } else if (j == 1) {
-                obj = items.uncommon;
-                return items.uncommon[Math.floor(Math.random() * obj.length)];
+                obj = itemObj.uncommon;
+                return itemObj.uncommon[Math.floor(Math.random() * obj.length)];
             } else if (j == 2) {
-                obj = items.rare;
-                return items.rare[Math.floor(Math.random() * obj.length)];
+                obj = itemObj.rare;
+                return itemObj.rare[Math.floor(Math.random() * obj.length)];
             } else if (j == 3) {
-                obj = items.veryrare;
-                return items.veryrare[Math.floor(Math.random() * obj.length)];
+                obj = itemObj.veryrare;
+                return itemObj.veryrare[Math.floor(Math.random() * obj.length)];
             }
         }
     }
