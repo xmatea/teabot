d162bb7a7bd7735ade943ea0a45f6ceccf96dd81
matea <mateabjork@gmail.com>
2019-02-26 18:02:04 +0100

    update

---
+++ src/commands/lottery.js
@@ -0,0 +1,12 @@
+exports.meta = {
+    name: "lottery",
+    desc: "Try your luck at the lottery!",
+    module: "Fun",
+    enabled: false,
+    whitelisted: false
+  };
+  
+  exports.fn = function (client, message, args) {
+   
+  }
+  
